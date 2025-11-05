a079e627c8c928933ac5bdbfa09103994a4bb901
Abdullahhhe <bdllhh121@gmail.com>
2025-11-05 23:53:49 +0200

    تعديل الصفحة الرئيسية

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,21 +1,29 @@
 'use client';
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-export default function Home(){
-    const router=useRouter();
-    useEffect(()=>{
-        const storeUser=localStorage.getItem("user");
-        if(storeUser){
-            const user=JSON.parse(storeUser);
-            if(user.role==="merchant"){
-                router.push("/components/addProduct");
+
+export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const storeUser = localStorage.getItem("user");
+            if (storeUser) {
+                const user = JSON.parse(storeUser);
+                if (user.role === "merchant") {
+                    router.push("/addProduct");
+                } else if (user.role === "admin") {
+                    router.push("/dashboard");
+                } else {
+                    router.push("/homepage");
+                }
             }
-            else router.push("/components/HomePage")
         }
-    })
-    return(
+    }, []);
+
+    return (
         <div className="mt-[-20px] bg-gray-50">
-            يتم التحميل
+            يتم التحميل...
         </div>
-    )
+    );
 }
