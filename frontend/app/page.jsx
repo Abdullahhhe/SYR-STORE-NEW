e04822b42b91fb09674983f876f806de9f47a029
Abdullahhhe <bdllhh121@gmail.com>
2025-11-05 23:57:53 +0200

    تعديل الصفحة الرئيسية

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -18,6 +18,9 @@ export default function Home() {
                     router.push("/homepage");
                 }
             }
+            if(!storeUser) {
+                router.push("/homepage");
+            }
         }
     }, []);
 
